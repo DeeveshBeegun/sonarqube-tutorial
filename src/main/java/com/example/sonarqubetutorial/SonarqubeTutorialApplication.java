2bf9853e184dd148dd99ccbbf103f4ff62acd5f9
DeeveshBeegun <deeveshbeegun@gmail.com>
2024-04-02 21:38:46 +0400

    Initial Commit

---
+++ src/main/java/com/example/sonarqubetutorial/SonarqubeTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.example.sonarqubetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeTutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sonarqubetutorial/SonarqubeTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sonarqubetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarqubeTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
